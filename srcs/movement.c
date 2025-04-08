8e70da8cd90ed55e96b35f27c60ea6cd18e514cf
Enzo Vella <DriQii>
2025-04-08 09:35:27 +0200

    fix colision

--- srcs/movement.c
+++ srcs/movement.c
@@ -6,11 +6,34 @@
 /*   By: evella <evella@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 23:23:38 by evella            #+#    #+#             */
-/*   Updated: 2025/04/08 09:02:53 by evella           ###   ########.fr       */
+/*   Updated: 2025/04/08 09:34:43 by evella           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
+
+static void	ft_check_moove(t_data *data, float dx, float dy)
+{
+	if ((data->map[(int)(data->player.y + dy
+				* (data->player.speed / 2.0))][(int)(data->player.x + dx
+		* (data->player.speed / 2.0))] != '1')
+			&& (data->map[(int)(data->player.y + dy
+			* (data->player.speed))][(int)(data->player.x + dx
+			* (data->player.speed))] != '1')
+			&& (data->map[(int)(data->player.y + dy
+			* (data->player.speed + 0.05))][(int)(data->player.x + dx
+			* (data->player.speed + 0.05))] != '1')
+			&& (data->map[(int)(data->player.y + dy
+			* (data->player.speed + 0.1))][(int)(data->player.x + dx
+			* (data->player.speed + 0.1))] != '1')
+			&& (data->map[(int)(data->player.y + dy
+			* (data->player.speed + 0.15))][(int)(data->player.x + dx
+			* (data->player.speed + 0.15))] != '1'))
+	{
+		data->player.x += dx * data->player.speed;
+		data->player.y += dy * data->player.speed;
+	}
+}
 
 static void	calculate_delta(float *dx, float *dy, float pa)
 {
@@ -33,13 +56,7 @@ void	ft_moove_pos(t_data *data)
 		calculate_delta(&dx, &dy, data->player.pa - M_PI_2);
 	if (data->key_state[D] == 1)
 		calculate_delta(&dx, &dy, data->player.pa + M_PI_2);
-	if (data->map[(int)(data->player.y + dy
-			* (data->player.speed))][(int)(data->player.x + dx
-			* data->player.speed)] != '1')
-	{
-		data->player.x += dx * data->player.speed;
-		data->player.y += dy * data->player.speed;
-	}
+	ft_check_moove(data, dx, dy);
 }
 
 void	ft_moove_fov(t_data *data)
